eeb547a3377494fa656a68ec786d28c6f17a84be
Dennis Muturi <deynohmuturi@gmail.com>
2025-10-20 22:31:52 +0300

    Refactor routing module: enhance handler function execution, improve error handling, and streamline routing map methods

--- src/routing.rs
+++ src/routing.rs
@@ -1,57 +1,98 @@
-use std::{collections::HashMap};
+use std::{collections::HashMap, sync::Arc};
 
 use matchit::Router;
-use thiserror::Error;
 
-use crate::{extractor::{FromRequest, FromRoutingMap, IntoResponse}, parser::http_message_parser::Request, response_writer::Response};
-
+use crate::{
+    extractor::{FromRequest, FromRoutingMap, IntoResponse},
+    parser::http_message_parser::Request,
+    response::SendingResponse,
+};
 
 #[derive(Eq, Hash, PartialEq)]
-pub enum HttpVerb{
+pub enum HttpVerb {
     GET,
     POST,
     PUT,
     PATCH,
     DELETE,
     OPTIONS,
-    HEAD
+    HEAD,
 }
-pub trait HandlerFunction<Args>:Send + Sync + 'static{
-    fn execute(self,request:Request)->Result<Response,ExtractionError>;
-}
-pub struct RoutingMap<F>(HashMap<HttpVerb,Router<F>>);
+pub struct RoutingMap<F>(HashMap<HttpVerb, Router<F>>);
 
 impl<F> RoutingMap<F> {
-    pub fn new()->Self{
+    pub fn new() -> Self {
         Self(HashMap::new())
     }
-    pub fn add_handler<Args>(&mut self,http_verb:HttpVerb,handler:F,route:&'static str)->Result<(),matchit::InsertError>
-    where F:HandlerFunction<Args>
+    pub fn add_handler<Args>(
+        &mut self,
+        http_verb: HttpVerb,
+        handler: F,
+        route: &'static str,
+    ) -> Result<(), matchit::InsertError>
+    where
+    F: HandlerFunction<Args>,
     {
-        let router=self.0.entry(http_verb).or_default();
+        let router = self.0.entry(http_verb).or_default();
         router.insert(route, handler)?;
         Ok(())
     }
+    pub fn get_handler<Args>(&self,http_verb:&HttpVerb,route:&str) ->Option<F> 
+    where
+    F: HandlerFunction<Args>,
+    {
+        let method_route=self.0.get(http_verb)?;
+        let matched_route = method_route.at(route).ok()?;
+        let handler_function=matched_route.value;
+        Some(handler_function.clone())
+    }
+    pub fn get_method_router<Args>(&self,http_verb: &HttpVerb)->Option<&Router<F>>
+    where F: HandlerFunction<Args>{
+        self.0.get(http_verb)
+
+    }
 }
-impl<F,T1,T2,T3,T4> HandlerFunction<(T1,T2,T3,T4)> for F where
-T1:FromRequest,
-T2:FromRequest,
-T3:FromRequest,
-T4:FromRoutingMap,
-F:Fn(T1,T2,T3,T4)->Response+Send+Sync+'static,
+pub trait HandlerFunction<Args>: Send + Sync + 'static+Clone{
+    fn execute<F2, Args2>(self, request: Request, routing_map: Arc<RoutingMap<F2>>) -> SendingResponse
+    where
+        F2: HandlerFunction<Args2>;
+}
+impl<F, T1, T2, T3> HandlerFunction<(T1, T2, T3)> for F
+where
+    T1: FromRequest,
+    T2: FromRequest,
+    T3: FromRoutingMap,
+    F: Fn(T1, T2, T3) -> SendingResponse + Send + Sync + 'static + Clone,
 {
-    fn execute(self,request:Request)->Result<Response,ExtractionError> {
-        let t1=T1::from_request(&request)?;
-        let t2=T2::from_request(&request)?;
-        let t3=T3::from_request(&request)?;
+    fn execute<F2, Args>(
+        self,
+        request: Request,
+        routing_map: Arc<RoutingMap<F2>>,
+    ) -> SendingResponse
+    where
+        F2: HandlerFunction<Args>,
+    {
+        let t1 = match T1::from_request(&request) {
+            Ok(val) => val,
+            Err(err) => return err.into_response(),
+        };
+        let t2 = match T2::from_request(&request) {
+            Ok(val) => val,
+            Err(err) => return err.into_response(),
+        };
+        let t3 = match T3::from_routing_map(&request, Arc::clone(&routing_map)) {
+            Ok(val) => val,
+            Err(err) => return err.into_response(),
+        };
+        self(t1, t2, t3)
     }
 }
 
-#[derive(Error, Debug)]
-enum ExtractionError {
-    #[error("route handler not found")]
-    RoutingError,
-    #[error("failed to deserialize body json {0}")]
-    SerdeJsonError(#[from] serde_urlencoded::de::Error)
-}
-impl IntoResponse for ExtractionError{}
+// #[derive(Error, Debug)]
+// enum ExtractionError {
+//     #[error("route handler not found")]
+//     RoutingError,
+//     #[error("failed to deserialize body json {0}")]
+//     SerdeJsonError(#[from] serde_urlencoded::de::Error)
+// }
+// impl IntoResponse for ExtractionError{}
